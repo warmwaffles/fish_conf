04b3ebbf1e9641fd762f4449c724e0af452996b6
Matthew Johnston <warmwaffles@gmail.com>
2019-12-03 18:24:22 -0600

    Checkin new changes

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -5,21 +5,23 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate 'x'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 
 function fish_prompt --description 'Write out the prompt'
-  set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
-  set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
-  set -l prompt_symbol ''
-  switch "$USER"
-  case root toor
-    set prompt_symbol '#'
-  case '*'
-    set prompt_symbol '$'
-  end
-  printf "[%s@%s%s%s]%s%s " $USER (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol (__fish_git_prompt)
+    set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
+    set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
+    set -l prompt_symbol ''
+
+    switch "$USER"
+    case root toor
+        set prompt_symbol '#'
+    case '*'
+        set prompt_symbol '$'
+    end
+
+    printf "[%s%s%s@%s%s%s]%s%s " (set_color red) $USER (set_color normal) (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol (__fish_git_prompt)
 end
